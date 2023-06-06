c7a929b7e04a06ed77cbd49b0a3def84af7370e0
Hevellyn Borges <hevellyn.borges@aluno.ifsp.edu.br>
2023-06-06 11:06:01 -0300

    urls.py

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: 127.0.0.1:8000/polls/
     path("", views.index, name="index"),
-    path("sobre", views.sobre, name="sobre")
+
+ # ex: 127.0.0.1:8000/polls/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+
+  # ex: 127.0.0.1:8000/polls/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),  
 ]
