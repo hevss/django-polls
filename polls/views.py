35b120696949d2292fc30aefd60a01fd3f712380
Hevellyn Borges <hevellyn.borges@aluno.ifsp.edu.br>
2023-06-21 09:27:58 -0300

    views.py

--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
-
 from .models import Question
 
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {"latest_question_list": latest_question_list,
+    "meu nome": "Choi Yeonjun"
+    }
+    return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
     return HttpResponse(f"Essa é a pergunta de número {question_id}.")
