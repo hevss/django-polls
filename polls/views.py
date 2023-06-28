4135d8854333b5ef0b4cca466dc5141ed3b4f766
Hevellyn Borges <hevellyn.borges@aluno.ifsp.edu.br>
2023-06-28 08:22:17 -0300

    details

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Question
@@ -6,12 +7,16 @@ from .models import Question
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
     context = {"latest_question_list": latest_question_list,
-    "meu nome": "Choi Yeonjun"
+    "meu nome": "Hevellyn"
     }
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse(f"Essa é a pergunta de número {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
