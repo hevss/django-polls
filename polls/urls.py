bec54b70ab40b50a1248a16880f9d4b3a46890a5
Hevellyn Gaspar <hevellyn.borges@aluno.ifsp.edu.br>
2023-08-30 09:05:02 -0300

    question detail

--- polls/urls.py
+++ polls/urls.py
@@ -3,22 +3,24 @@ from . import views
 
 urlpatterns = [
 
-    #function based views
+#     #function based views
 
-    # ex: 127.0.0.1:8000/polls/
-    path("", views.index, name="index"),
+#     # ex: 127.0.0.1:8000/polls/
+#     path("", views.index, name="index"),
 
- # ex: 127.0.0.1:8000/polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
+#  # ex: 127.0.0.1:8000/polls/5/
+#     path("<int:question_id>/", views.detail, name="detail"),
 
-# ex: 127.0.0.1:8000/polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
+# # ex: 127.0.0.1:8000/polls/5/results/
+#     path("<int:question_id>/results/", views.results, name="results"),
 
-# ex: 127.0.0.1:8000/polls/5/vote/
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+# # ex: 127.0.0.1:8000/polls/5/vote/
+#     path("<int:question_id>/vote/", views.vote, name="vote"),
 
     #class based views
+    # ex: localhost:8000/enquetes/listar
     path('listar', views.QuestionListView.as_view(), name="question-list"),
 # página de cadastro de nova enquete
-    path('cadastrar', views.QuestionCreateView.as_view(), name="question-create")
+    path('cadastrar', views.QuestionCreateView.as_view(), name="question-create"),
+    path('int:pk>', views.QuestionDetailView.as_view(), name="question-detail")
 ]
